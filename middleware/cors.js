302f07412ddf1b620727b7b06fb2226f39def876
alessandropcostabr <alessandropcostabr@gmail.com>
2025-09-09 20:47:37 -0300

    Expand CORS origin and headers

--- middleware/cors.js
+++ middleware/cors.js
@@ -16,7 +16,7 @@ const corsOptions = {
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'X-CSRF-Token']
 };
 
 module.exports = cors(corsOptions);
