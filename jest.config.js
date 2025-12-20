727681f6cd3302f466bfde02b3095fe6566bdab7
Alessandro Costa <alessandropcostabr@gmail.com>
2025-12-20 12:06:28 -0300

    fix(test): ignore __tests__/setup in Jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   // Para projetos em JS puro, nenhum preset é necessário
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['<rootDir>/__tests__/setup/'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   setupFilesAfterEnv: [],
